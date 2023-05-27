9824e5f621b8be74ce955d9b8864925ed9948998
Lob26 <p.lobato@uniandes.edu.co>
2023-05-27 14:05:01 -0500

    Goaty inserts

--- Proyecto/Iteracion4uwu/st-alohandes-jdo/src/main/python/populator.py
+++ Proyecto/Iteracion4uwu/st-alohandes-jdo/src/main/python/populator.py
@@ -18,14 +18,6 @@ class Populator:
 
     def __init__(self):
         cx_Oracle.init_oracle_client(lib_dir=r"C:\Oracle\instantclient_21_10")
-        config_loaded = self.__load_json_config()
-        if config_loaded:
-            print("Bien")
-        else:
-            print("Mal")
-
-    def __load_json_config(self) -> bool:
-        return True
 
     def populate_a_usuario(self):
         connection = self.get_connection()
@@ -36,8 +28,10 @@ class Populator:
             tipoid = self.fake.random_choices(elements=('CARNET_U', 'CEDULA', 'PASAPORTE'))
             login = self.fake.user_name()
             relacionu = self.fake.word()
-            cursor.execute(
-                f"INSERT INTO a_usuario (id,tipoid,login,relacionu) VALUES ({id},'{tipoid}','{login}','{relacionu}')")
+
+            insert_query = f"INSERT INTO a_usuario (id, tipoid, login, relacionu) VALUES ({id}, '{tipoid}', '{login}'," \
+                           f" '{relacionu}')"
+            cursor.execute(insert_query)
             insertados.append(id)
         connection.commit()
         connection.close()
@@ -52,8 +46,8 @@ class Populator:
             vencimiento_rnt = self.fake.date_between(start_date='-1y', end_date='today')
             registro_super_turismo = self.fake.word()
             vencimiento_registro_st = self.fake.date_between(start_date='-1y', end_date='today')
-            categoria = self.fake.random_element(elements=('HOTEL', 'HOSTAL', 'P_NATURAL', 'APARTAMENTO', 'VECINOS',
-                                                           'VIVIENDA_U'))
+            categoria = self.fake.random_element(
+                elements=('HOTEL', 'HOSTAL', 'P_NATURAL', 'APARTAMENTO', 'VECINOS', 'VIVIENDA_U'))
             direccion = self.fake.address().replace("'", "''")
             hora_apertura = self.fake.time_object(end_datetime=None).time()
             hora_cierre = self.fake.time_object(end_datetime=None).time()
@@ -61,15 +55,15 @@ class Populator:
             ganancia_anio_actual = self.fake.pydecimal(left_digits=5, right_digits=2, positive=True)
             ganancia_anio_corrido = self.fake.pydecimal(left_digits=5, right_digits=2, positive=True)
 
-            cursor.execute(f"INSERT INTO a_operador (id, numero_rnt, vencimiento_rnt, registro_super_turismo, " +
-                           f"vencimiento_registro_st, categoria, direccion, hora_apertura, hora_cierre, tiempo_minimo, " +
-                           f"ganancia_anio_actual, ganancia_anio_corrido)" +
-                           f" VALUES ('{id}', {numero_rnt}, TO_DATE('{vencimiento_rnt}', 'YYYY-MM-DD')," +
-                           f" '{registro_super_turismo}', TO_DATE('{vencimiento_registro_st}', 'YYYY-MM-DD')," +
-                           f" '{categoria}', '{direccion}', TO_DATE('{hora_apertura}', 'HH24:MI:SS'),"
-                           f" TO_DATE('{hora_cierre}','HH24:MI:SS'), {tiempo_minimo}, {ganancia_anio_actual},"
-                           f" {ganancia_anio_corrido})")
-
+            insert_query = f"INSERT INTO a_operador (id, numero_rnt, vencimiento_rnt, registro_super_turismo," \
+                           f" vencimiento_registro_st, categoria, direccion, hora_apertura, hora_cierre, tiempo_minimo," \
+                           f" ganancia_anio_actual, ganancia_anio_corrido) " \
+                           f"VALUES ({id}, {numero_rnt}, TO_DATE('{vencimiento_rnt}', 'YYYY-MM-DD')," \
+                           f" '{registro_super_turismo}', TO_DATE('{vencimiento_registro_st}', 'YYYY-MM-DD')," \
+                           f" '{categoria}', '{direccion}', TO_DATE('{hora_apertura}', 'HH24:MI:SS')," \
+                           f" TO_DATE('{hora_cierre}','HH24:MI:SS'), {tiempo_minimo}, {ganancia_anio_actual}," \
+                           f" {ganancia_anio_corrido})"
+            cursor.execute(insert_query)
         connection.commit()
         connection.close()
 
@@ -88,11 +82,11 @@ class Populator:
             habilitada = self.fake.random_int(min=0, max=1)
             operador = choice(usuarios)
 
-            cursor.execute(
-                f"INSERT INTO a_oferta (id, capacidad, precio, tamanio, dias_reservados, fecha_creacion, piso," +
-                f" habilitada, operador) "
-                f"VALUES ({id}, {capacidad}, {precio}, {tamanio}, {dias_reservados}," +
-                f" TO_DATE('{fecha_creacion}', 'YYYY-MM-DD'), {piso}, {habilitada}, '{operador}')")
+            insert_query = f"INSERT INTO a_oferta (id, capacidad, precio, tamanio, dias_reservados, fecha_creacion," \
+                           f" piso, habilitada, operador) " \
+                           f"VALUES ({id}, {capacidad}, {precio}, {tamanio}, {dias_reservados}," \
+                           f" TO_DATE('{fecha_creacion}', 'YYYY-MM-DD'), {piso}, {habilitada}, '{operador}')"
+            cursor.execute(insert_query)
             insertados.append(id)
 
         connection.commit()
@@ -110,9 +104,11 @@ class Populator:
             cantidad = self.fake.random_int(min=1, max=10)
             tipo = self.fake.random_element(elements=('APARTAMENTO', 'HABITACION'))
             cliente = self.fake.random_int(min=1, max=10000)
-            cursor.execute(
-                f"INSERT INTO a_reservacolectiva (id, fecha_inicio, duracion, cantidad, tipo, cliente) VALUES ({id}," +
-                f" '{fecha_inicio}', {duracion}, {cantidad}, '{tipo}', {cliente})")
+
+            insert_query = f"INSERT INTO a_reservacolectiva (id, fecha_inicio, duracion, cantidad, tipo, cliente) " \
+                           f"VALUES ({id}, TO_DATE('{fecha_inicio}', 'YYYY-MM-DD'), {duracion}, {cantidad}," \
+                           f" '{tipo}', {cliente})"
+            cursor.execute(insert_query)
             insertados[i] = id
         connection.commit()
         connection.close()
@@ -129,23 +125,27 @@ class Populator:
             fin_cancelacion_oportuna = self.fake.date_between(start_date='-30d', end_date=fecha_inicio)
             porcentaje_a_pagar = self.fake.random_int(min=0, max=100)
             monto_total = self.fake.random_int(min=100, max=1000)
-            propiedad = choice(ofertas)
+            propiedad = choice(ofertas)[0]
             colectiva = choice(colectivas)[0]
 
-            cursor.execute(
-                f"INSERT INTO a_reserva (id, fecha_inicio, fecha_fin, personas, fin_cancelacion_oportuna," +
-                f" porcentaje_a_pagar, monto_total, propiedad, colectiva)" +
-                f"VALUES ({id}, '{fecha_inicio}', '{fecha_fin}', {personas}, '{fin_cancelacion_oportuna}'," +
-                f"{porcentaje_a_pagar}, {monto_total}, {propiedad}, {colectiva})")
+            insert_query = f"INSERT INTO a_reserva (id, fecha_inicio, fecha_fin, personas, fin_cancelacion_oportuna," \
+                           f" porcentaje_a_pagar, monto_total, propiedad, colectiva) " \
+                           f"VALUES ({id}, TO_DATE('{fecha_inicio}', 'YYYY-MM-DD'), TO_DATE('{fecha_fin}', 'YYYY-MM-DD')," \
+                           f" {personas}, TO_DATE('{fin_cancelacion_oportuna}', 'YYYY-MM-DD'), {porcentaje_a_pagar}," \
+                           f" {monto_total}, {propiedad}, {colectiva})"
+            cursor.execute(insert_query)
 
         connection.commit()
         connection.close()
 
     def get_connection(self):
-        dsn_tns = cx_Oracle.makedsn('fn4.oracle.virtual.uniandes.edu.co', '1521', service_name='prod') # if needed, place an 'r' before any parameter in order to address special characters such as '\'.
-        conn = cx_Oracle.connect(user=r'ISIS2304B28202310', password='LbyCVTywUGkk', dsn=dsn_tns) # if needed, place an 'r' before any parameter in order to address special characters such as '\'. For example, if your user name contains '\', you'll need to place 'r' before the user name: user=r'User Name'
+        dsn_tns = cx_Oracle.makedsn('fn4.oracle.virtual.uniandes.edu.co', '1521',
+                                    service_name='prod')  # if needed, place an 'r' before any parameter in order to address special characters such as '\'.
+        conn = cx_Oracle.connect(user=r'ISIS2304B28202310', password='LbyCVTywUGkk',
+                                 dsn=dsn_tns)  # if needed, place an 'r' before any parameter in order to address special characters such as '\'. For example, if your user name contains '\', you'll need to place 'r' before the user name: user=r'User Name'
         return conn
-    
+
+
 populator = Populator()
 connection = populator.get_connection()
 cursor = connection.cursor()
